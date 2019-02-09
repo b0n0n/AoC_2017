6f97c1f201567d452c8063c8bbc6a7579e0e5748
Sparrow <banana@b0n0n.com>
2019-02-09 00:05:30 -0800

    add day 10 solution

--- 10/10.rb
+++ 10/10.rb
@@ -1,4 +1,40 @@
 # coding: utf-8
-=begin
+# input = [3, 4, 1, 5]
+# l = [0, 1, 2, 3, 4]
 
-=end
+seq = File.open("data").read.strip.split(",").map(&:to_i)
+l = [*0..255]
+
+t_r = 0
+
+seq.each_with_index { |r_l, s_l|
+	l[0, r_l] = l[0, r_l].reverse
+	t_r += r_l+s_l
+	l.rotate!(r_l+s_l)
+}
+l.rotate!(-(t_r%l.size))
+
+
+puts "part1 => #{l[0]*l[1]}"
+
+# ==== part2 ====
+# seq = "1,2,3".bytes
+
+suffix = [17, 31, 73, 47, 23]
+seq = File.open("data").read.strip.bytes
+seq.push(*suffix)
+l = [*0..255]
+
+t_r = 0
+
+seq.cycle(64).each_with_index { |r_l, s_l|
+	l[0, r_l] = l[0, r_l].reverse
+	t_r += r_l+s_l
+	l.rotate!(r_l+s_l)
+}
+l.rotate!(-(t_r%l.size))
+
+hash = ""
+l.each_slice(16) { |i| hash += i.reduce(:^).to_s(16)}
+
+puts "part2 => #{hash}"
