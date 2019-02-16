e5be426f1a2cebe0f2341d091da84157b09f12c9
Sparrow <banana@b0n0n.com>
2019-02-16 00:09:12 -0800

    add day 15 part 2

--- 15/15.rb
+++ 15/15.rb
@@ -11,15 +11,12 @@ factor_A = 16807
 factor_B = 48271
 
 pairs = 40000000
+total = 0
 g = 2147483647
 
 sq1 = 618
 sq2 = 814
 
-sq1 = 65
-sq2 = 8921
-
-total = 0
 
 pairs.times { |i|
 	sq1 = ((sq1 * factor_A) % g)
@@ -33,24 +30,25 @@ puts "part1=>#{total}"
 # part 2
 
 pairs = 5000000
-pairs = 1060
 total = 0
 
-sq1 = 65
-sq2 = 8921
+sq1 = 618
+sq2 = 814
 
 sl1, sl2 = [], []
 
-pairs.times { |i|
+loop do
 	sq1 = (sq1 * factor_A) % g
 	sq2 = (sq2 * factor_B) % g
 
-	sl1 << sq1%0x100 if sq1 % 4 == 0
-	sl2 << sq2%0x100 if sq2 % 4 == 0
-}
+	sl1 << sq1%0x10000 if sq1 % 4 == 0
+	sl2 << sq2%0x10000 if sq2 % 8 == 0
 
-[sl1.size, sl2.size].min.times { |i|
+	break if [sl1.size, sl2.size].min == pairs
+end
+
+pairs.times { |i|
 	total += 1 if sl1[i] == sl2[i]
 }
-p sl1[-3..], sl2[-3..]
+
 puts "part2=>#{total}"
